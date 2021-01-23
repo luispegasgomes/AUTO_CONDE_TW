43c68cf4581426a95ac5b8bc539729a52e4d6714
luispegasgomes <luispegasgomes@gmail.com>
2021-01-23 18:59:57 +0000

    validação login  java

--- js/login.js
+++ js/login.js
@@ -1,4 +1,21 @@
-function login(){
-    window.alert('Início de sessão efetuado com sucesso!')
+let utilizadores = [
+    {"login": "luisgomes", "senha": "gomes"},
+    {"login": "fabiofernades", "senha": "fernades"},
+    {"login": "joaosantos", "senha": "santos"},
+];
+
+function login() {
+    let utilizador = document.getElementsByName('username')[0].value.toLowerCase();
+    let senha = document.getElementsByName('pw')[0].value;
+
+    for (let u in utilizadores) {
+        let us = utilizadores[u];
+        if (us.login === utilizador && us.senha === senha) {
+            alert('Início de sessão efetuado com sucesso!')
+            window.location = "autoconde.html";
+            return true;        
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+    return false;
 }
-    
